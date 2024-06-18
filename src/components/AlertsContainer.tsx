1e1c4c268d031510f8af75ce2d23f93884a0c84a
mmarian <mihailmarian12@gmail.com>
2024-06-18 18:25:23 +0100

    user not found state

--- src/components/AlertsContainer.tsx
+++ src/components/AlertsContainer.tsx
@@ -6,18 +6,29 @@ const API_HOST =
     ? "https://socialalerts.app.taralys.com"
     : "http://127.0.0.1:8000";
 
+enum ApiResponseState {
+  Initial,
+  IsLoading,
+  DataReceived,
+  UsernameNotFound,
+  Error,
+}
+
 function AlertsContainer() {
   const [username, setUsername] = useState("");
-  const [alertsReceived, setAlertsReceived] = useState<Alerts>();
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<Error>();
+  const [alertsReceived, setAlertsReceived] = useState<Alerts>({
+    comment_replies: [],
+    post_comments: [],
+  });
+  const [apiError, setApiError] = useState<Error>(new Error());
+  const [apiResponseState, setApiResponseState] = useState(
+    ApiResponseState.Initial
+  );
 
   const submitHandler = (event: FormEvent) => {
     event.preventDefault();
 
-    setAlertsReceived(undefined);
-    setIsLoading(true);
-    setError(undefined);
+    setApiResponseState(ApiResponseState.IsLoading);
 
     fetch(`${API_HOST}/alerts/users/${username}`, {
       method: "GET",
@@ -27,22 +38,25 @@ function AlertsContainer() {
       },
     })
       .then((response) => {
+        if (response.status == 404) {
+          setApiResponseState(ApiResponseState.UsernameNotFound);
+          return;
+        }
+
         if (response.status != 200) {
           throw Error(
             `Expected response status 200, got ${response.status}: ${response.statusText}`
           );
         }
 
-        return response.json();
-      })
-      .then((data: Alerts) => {
-        setAlertsReceived(data);
+        response.json().then((data: Alerts) => {
+          setApiResponseState(ApiResponseState.DataReceived);
+          setAlertsReceived(data);
+        });
       })
       .catch((error: Error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
+        setApiResponseState(ApiResponseState.Error);
+        setApiError(error);
       });
   };
 
@@ -59,17 +73,33 @@ function AlertsContainer() {
         />
         <button
           className={`ml-5 text-white font-bold py-2 px-4 rounded w-40 ${
-            isLoading
+            apiResponseState === ApiResponseState.IsLoading
               ? "bg-gray-700 hover:bg-gray-700"
               : "bg-blue-500 hover:bg-blue-700"
           }`}
           type="submit"
-          disabled={isLoading}
+          disabled={apiResponseState === ApiResponseState.IsLoading}
         >
           Get alerts
         </button>
       </form>
-      {error && (
+      {apiResponseOutput(apiResponseState, apiError, alertsReceived)}
+    </>
+  );
+}
+
+const apiResponseOutput = (
+  state: ApiResponseState,
+  error: Error,
+  alertsReceived: Alerts
+) => {
+  switch (state) {
+    case ApiResponseState.IsLoading:
+      return <p className="font-bold">Loading...</p>;
+    case ApiResponseState.UsernameNotFound:
+      return <p className="text-red-700 font-semibold">Username not found</p>;
+    case ApiResponseState.Error:
+      return (
         <p className="text-red-700 font-semibold">
           Something went wrong, error message{" "}
           <span className="inline-block">'{error.message}'</span>. Please reach
@@ -82,9 +112,9 @@ function AlertsContainer() {
           </a>{" "}
           for help.
         </p>
-      )}
-      {isLoading && <p className="font-bold">Loading...</p>}
-      {alertsReceived && (
+      );
+    case ApiResponseState.DataReceived:
+      return (
         <div className="space-y-7">
           <Alert type="post_comments" items={alertsReceived.post_comments} />
           <Alert
@@ -92,9 +122,10 @@ function AlertsContainer() {
             items={alertsReceived.comment_replies}
           />
         </div>
-      )}
-    </>
-  );
-}
+      );
+    default:
+      return <></>;
+  }
+};
 
 export default AlertsContainer;
