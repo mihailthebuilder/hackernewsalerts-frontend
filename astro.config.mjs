c04ebefb806a7ac7a1d47bda13b310b309f888bf
Mihail Marian <m.marian@elsevier.com>
2024-06-19 14:11:06 +0100

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: "https://hackernewsalerts.com",
+  integrations: [tailwind(), react()],
 });
