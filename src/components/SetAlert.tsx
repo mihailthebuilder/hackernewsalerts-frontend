a1b0f8b685807bbe3056495224ed8d90daf1191e
Mihail Marian <m.marian@elsevier.com>
2024-06-25 14:48:06 +0100

    error handling for set alert

--- src/components/SetAlert.tsx
+++ src/components/SetAlert.tsx
@@ -57,34 +57,59 @@ export default function SetAlert() {
   }
 
   return (
-    <form onSubmit={submitHandler} className="space-y-3">
-      <input
-        className="border border-black w-full block rounded focus:ring-cyan-800 p-2 col-span-6"
-        type="text"
-        placeholder="HN username"
-        onChange={(event) => setUsername(event.target.value.toLowerCase())}
-        value={username}
-        required
-      />
-      <input
-        className="border border-black w-full block rounded focus:ring-cyan-800 p-2 col-span-6"
-        type="email"
-        placeholder="email"
-        onChange={(event) => setEmail(event.target.value.toLowerCase())}
-        value={email}
-        required
-      />
-      <button
-        className={`text-white font-bold py-2 px-4 rounded w-40 ${
-          apiResponseState === ApiResponseState.IsLoading
-            ? "bg-gray-700 hover:bg-gray-700"
-            : "bg-blue-700 hover:bg-blue-900"
-        }`}
-        type="submit"
-        disabled={apiResponseState === ApiResponseState.IsLoading}
-      >
-        Sign up
-      </button>
-    </form>
+    <>
+      <form onSubmit={submitHandler} className="space-y-3 mb-3">
+        <input
+          className="border border-black w-full block rounded focus:ring-cyan-800 p-2 col-span-6"
+          type="text"
+          placeholder="HN username"
+          onChange={(event) => setUsername(event.target.value.toLowerCase())}
+          value={username}
+          required
+        />
+        <input
+          className="border border-black w-full block rounded focus:ring-cyan-800 p-2 col-span-6"
+          type="email"
+          placeholder="email"
+          onChange={(event) => setEmail(event.target.value.toLowerCase())}
+          value={email}
+          required
+        />
+        <button
+          className={`text-white font-bold py-2 px-4 rounded w-40 ${
+            apiResponseState === ApiResponseState.IsLoading
+              ? "bg-gray-700 hover:bg-gray-700"
+              : "bg-blue-700 hover:bg-blue-900"
+          }`}
+          type="submit"
+          disabled={apiResponseState === ApiResponseState.IsLoading}
+        >
+          Sign up
+        </button>
+      </form>
+      {apiNonSuccessOutput(apiResponseState, apiError)}
+    </>
   );
 }
+
+const apiNonSuccessOutput = (state: ApiResponseState, error: Error) => {
+  switch (state) {
+    case ApiResponseState.IsLoading:
+      return <p className="font-bold">Loading...</p>;
+    case ApiResponseState.UsernameNotFound:
+      return <p className="text-red-700 font-semibold">Username not found</p>;
+    case ApiResponseState.Error:
+      return (
+        <p className="text-red-700 font-semibold">
+          Something went wrong, error message{" "}
+          <span className="inline-block">'{error.message}'</span>. Check the{" "}
+          <a href="#contact" className="underline inline-block">
+            contact section
+          </a>{" "}
+          to see how you can reach out to me for help.
+        </p>
+      );
+    default:
+      return <></>;
+  }
+};
