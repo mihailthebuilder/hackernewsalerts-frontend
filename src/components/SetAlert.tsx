d83eea6f8823194cdb0ce2d3d785254738a5bb32
mmarian <mihailmarian12@gmail.com>
2024-06-25 14:16:25 +0100

    copied api request handling

--- src/components/SetAlert.tsx
+++ src/components/SetAlert.tsx
@@ -3,19 +3,51 @@ import { type FormEvent, useState } from "react";
 enum ApiResponseState {
   Initial,
   IsLoading,
-  DataReceived,
+  Success,
   UsernameNotFound,
   Error,
 }
 
+const API_HOST = "https://socialalerts.app.taralys.com";
+// const API_HOST = "http://127.0.0.1:8000";
+
 export default function SetAlert() {
   const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [apiResponseState, setApiResponseState] = useState(
     ApiResponseState.Initial
   );
+  const [apiError, setApiError] = useState<Error>(new Error());
 
   const submitHandler = (event: FormEvent) => {
     event.preventDefault();
+    setApiResponseState(ApiResponseState.IsLoading);
+
+    fetch(`${API_HOST}/alerts/users/${username}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "text/plain",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status == 404) {
+          setApiResponseState(ApiResponseState.UsernameNotFound);
+          return;
+        }
+
+        if (response.status != 200) {
+          throw Error(
+            `Expected response status 200, got ${response.status}: ${response.statusText}`
+          );
+        }
+
+        setApiResponseState(ApiResponseState.Success);
+      })
+      .catch((error: Error) => {
+        setApiResponseState(ApiResponseState.Error);
+        setApiError(error);
+      });
   };
 
   return (
@@ -32,8 +64,8 @@ export default function SetAlert() {
         className="border border-black w-full block rounded focus:ring-cyan-800 p-2 col-span-6"
         type="email"
         placeholder="email"
-        onChange={(event) => setUsername(event.target.value.toLowerCase())}
-        value={username}
+        onChange={(event) => setEmail(event.target.value.toLowerCase())}
+        value={email}
         required
       />
       <button
