ab18aaaf3591b1713bc254fc9072518df701c142
Mihail Marian <m.marian@elsevier.com>
2024-06-25 13:57:45 +0100

    styles for inputs

--- src/components/SetAlert.tsx
+++ src/components/SetAlert.tsx
@@ -1,3 +1,52 @@
+import { type FormEvent, useState } from "react";
+
+enum ApiResponseState {
+  Initial,
+  IsLoading,
+  DataReceived,
+  UsernameNotFound,
+  Error,
+}
+
 export default function SetAlert() {
-  return <div></div>;
+  const [username, setUsername] = useState("");
+  const [apiResponseState, setApiResponseState] = useState(
+    ApiResponseState.Initial
+  );
+
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={submitHandler} className="space-y-3">
+      <input
+        className="border border-black w-full block rounded focus:ring-cyan-800 p-2 col-span-6"
+        type="text"
+        placeholder="HN username"
+        onChange={(event) => setUsername(event.target.value.toLowerCase())}
+        value={username}
+        required
+      />
+      <input
+        className="border border-black w-full block rounded focus:ring-cyan-800 p-2 col-span-6"
+        type="email"
+        placeholder="email"
+        onChange={(event) => setUsername(event.target.value.toLowerCase())}
+        value={username}
+        required
+      />
+      <button
+        className={`text-white font-bold py-2 px-4 rounded w-40 ${
+          apiResponseState === ApiResponseState.IsLoading
+            ? "bg-gray-700 hover:bg-gray-700"
+            : "bg-blue-700 hover:bg-blue-900"
+        }`}
+        type="submit"
+        disabled={apiResponseState === ApiResponseState.IsLoading}
+      >
+        Sign up
+      </button>
+    </form>
+  );
 }
