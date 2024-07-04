b074cb88d04afba32d4da71431c76d51e088b2c5
Mihail Marian <m.marian@elsevier.com>
2024-07-04 15:48:06 +0100

    mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hackernewsalerts.com",
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [tailwind(), react(), sitemap(), mdx()]
 });
